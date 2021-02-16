7c35f98f6e31f391449aeedf816ec81bccdccc2f
p0tatoes <76986919+p0tatoes@users.noreply.github.com>
2021-02-16 16:41:10 +0800

    Finished Midnight Outcome

--- app/src/main/java/com/example/lastnight/MidnightOutcome.java
+++ app/src/main/java/com/example/lastnight/MidnightOutcome.java
@@ -25,7 +25,6 @@ public class MidnightOutcome extends AppCompatActivity implements View.OnClickLi
         txtOutcome = findViewById(R.id.txt_MidnightOutcome);
 
         Intent next = getIntent();
-        Intent lighter_Prologue = getIntent();
 
         decision_Midnight = next.getCharExtra("decision_midnight", ' ');
         bringLighter = getIntent().getBooleanExtra("BROUGHT_LIGHTER", true);
@@ -56,19 +55,46 @@ public class MidnightOutcome extends AppCompatActivity implements View.OnClickLi
                 else if (decision_Midnight == 'a' && page == 4) {
                     txtOutcome.setText("They say that they can give you some of their extra supplies.");
                 }
-                else if (decision_Midnight == 'a' && page == 4) {
+                else if (decision_Midnight == 'a' && page == 5) {
                     txtOutcome.setText("But, under the condition that you also give them something valuable in exchange.");
                 }
-                else if (decision_Midnight == 'a' && page == 5) {
-                    txtOutcome.setText(" ");
-                    if (bringLighter == false) {
+                else if (decision_Midnight == 'a') {
+                    if (!bringLighter && page == 6) {
                         txtOutcome.setText("You check your bag, but did not find anything of value to offer.");
                     }
-                    else if (bringLighter == true) {
+                    else if (bringLighter && page == 6) {
                         txtOutcome.setText("You check your bag and decided to offer your lighter.");
                     }
-
+                    else if (!bringLighter && page == 7) {
+                        txtOutcome.setText("The group tells you to leave and never approach them if you don't have anything of value to offer.");
+                    }
+                    else if (bringLighter && page == 7) {
+                        txtOutcome.setText("They accepted the offer.");
+                    }
+                    else if (!bringLighter && page == 8) {
+                        txtOutcome.setText("Disappointed, you continued with your journey.");
+                    }
+                    else if (bringLighter && page == 8) {
+                        txtOutcome.setText("They expressed their gratitude...");
+                    }
+                    else if (!bringLighter && page == 9) {
+                        startActivity(third_Night);
+                    }
+                    else if (bringLighter && page == 9) {
+                        txtOutcome.setText("and told you that they were having trouble starting a fire.");
+                    }
+                    else if (bringLighter && page == 10) {
+                        txtOutcome.setText("They welcomed you into the camp and gave you enough supplies to last through the remaining journey.");
+                    }
+                    else if (bringLighter && page == 11) {
+                        txtOutcome.setText("After gathering and preparing your supplies, you continued your journey");
+                    }
+                    else if (bringLighter && page == 12) {
+                        startActivity(third_Night);
+                    }
                 }
+
+
 
                 break;
         }
